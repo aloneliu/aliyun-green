0b56947297dc2112092e63e55420b49d156d8ddc
aloneliu <745302601@qq.com>
2017-04-12 15:21:07 +0800

    init

---
+++ src/Colation.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Liu\AliyunGreen;
+// use Green\Request\V20161216 as Green;
+
+class Colation
+{
+    public function __construct()
+    {
+        echo 'construct';
+    }
+
+    /**
+     *
+     * 纯文本垃圾检测接口
+     */
+    public function text()
+    {
+        echo 'Colation-text';
+    }
+
+    /**
+     *
+     * 图片垃圾检测接口
+     */
+    public function img()
+    {
+        echo 'Colation-Colation';
+    }
+
+
+
+}
