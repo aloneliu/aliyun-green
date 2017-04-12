9ba42bf027f28e9cde12e5e556a8e09fcfe6809c
aloneliu <745302601@qq.com>
2017-04-12 16:14:47 +0800

    1

--- src/Colation.php
+++ src/Colation.php
@@ -1,13 +1,23 @@
 <?php
 
 namespace Liu\AliyunGreen;
-// use Green\Request\V20161216 as Green;
+
+use Green\Request\V20161216 as Green;
 
 class Colation
 {
-    public function __construct()
+    protected $iClientProfile;
+
+    protected $client;
+
+
+    public function __construct($accessKeyId = null, $accessKeySecret = null)
     {
-        echo 'construct';
+        // accessKeyId、accessKeySecret
+        $this->iClientProfile = \DefaultProfile::getProfile("cn-hangzhou", $accessKeyId, $accessKeySecret);
+        \DefaultProfile::addEndpoint("cn-hangzhou", "cn-hangzhou", "Green", "green.cn-hangzhou.aliyuncs.com");
+
+        $this->client = new \DefaultAcsClient($this->iClientProfile);
     }
 
     /**
@@ -16,7 +26,41 @@ class Colation
      */
     public function text()
     {
-        echo 'Colation-text---';
+        $request = new Green\TextAntispamDetectionRequest();
+
+        $dataItem1 = [
+            'dataId'   => uniqid(), # 可填,如果未填,系统自动生成一个
+            'content'  => '小阿诗在等你:女生 就我对面一栋宿舍', #发帖内容, 必填
+            'postId'   => '111', # 发帖用户id 可选
+            'postTime' => round(microtime(true) * 1000) # 发帖时间, 可选
+        ];
+        $dataItem2 = [
+            'dataId'   => uniqid(), # 可填,如果未填,系统自动生成一个
+            'content'  => '你好牛', # 发帖内容, 必填
+            'postId'   => '111', # 发帖用户id, 可选, postId与postTime需同时填后者不填
+            'postTime' => round(microtime(true) * 1000) # 发帖时间,可选, postId与postTime需同时填后者不填
+        ];
+
+        //  设置要检测的文本, 可以同时检测最多50个文本
+        $request->setDataItems(json_encode([$dataItem1, $dataItem2]));
+
+        try {
+            $response = $this->client->getAcsResponse($request);
+            print_r($response);
+            // 如果成功且命中
+            if ("Success" == $response->Code) {
+                $textAntispamResults = $response->TextAntispamResults->TextAntispamResult;
+                foreach ($textAntispamResults as $textAntispamResult) {
+                    // 原文本
+                    print_r($textAntispamResult->Text);
+                    // 是否是垃圾
+                    print_r($textAntispamResult->IsSpam);
+                    print_r("\n");
+                }
+            }
+        } catch (Exception $e) {
+            print_r($e);
+        }
     }
 
     /**
@@ -27,7 +71,6 @@ class Colation
     {
         echo 'Colation-Colation';
     }
-
 
 
 }
